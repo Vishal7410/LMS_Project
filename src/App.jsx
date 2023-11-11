af85cb360a424094b9263dcb3aabe30da1ef98d0
Vishal7410 <vishalpardhi2001@gmail.com>
2023-11-12 00:02:06 +0530

    eslint added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
+
+import { Routes } from 'react-router-dom';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
-   LMS
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <Routes>
+    {/* <Route path="/" element = {<Home/>} ></Route> */ }
+   </Routes>
+  
 
    </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import './index.css';
+
+//Library Imports
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App.jsx';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
-  
+ <BrowserRouter>
+ <App />
+ <Toaster/>
+ </BrowserRouter>  
 )
